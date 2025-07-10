1269c3b76950b0d5d60420e67e9a6cf5d44a43dd
GWONSEOYEON <sykwon1004@naver.com>
2025-07-10 13:15:09 +0900

    Object Pooling
    Bullet Object 재활용
    Queue 기능 사용 연습

---
+++ Assets/1. Data Structure/02. Scripts/Object Pool/PoolObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+    public class PoolObject : MonoBehaviour
+    {
+        private ObjectPoolQueue pool;
+
+        public float bulletSpeed = 100f;
+
+        private void Awake()
+        {
+            pool = FindFirstObjectByType<ObjectPoolQueue>();
+        }
+
+        private void OnEnable()
+        {
+            Invoke("ReturnPool", 3f);
+        }
+
+        private void Update()
+        {
+            transform.position += Vector3.forward * Time.deltaTime * bulletSpeed;
+        }
+
+
+    void ReturnPool()
+        {
+            pool.EnqueueObject(gameObject);
+
+    }
+
+}
