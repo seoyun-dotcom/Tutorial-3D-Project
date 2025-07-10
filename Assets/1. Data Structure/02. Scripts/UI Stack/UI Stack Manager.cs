dec72cb8feea1786bd814d30606e177e77000d5f
GWONSEOYEON <sykwon1004@naver.com>
2025-07-10 10:53:03 +0900

    UI Stack, HandleBar

---
+++ Assets/1. Data Structure/02. Scripts/UI Stack/UI Stack Manager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class UIStackManager : MonoBehaviour
+{
+    public Stack<GameObject> uiStack = new Stack<GameObject>();
+
+    public Button[] buttons;
+    public GameObject[] popupUIs;
+
+    private void Start()
+    {
+        buttons[0].onClick.AddListener(PopupOn1);
+        buttons[1].onClick.AddListener(PopupOn2);
+        buttons[2].onClick.AddListener(PopupOn3);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameObject currUI = uiStack.Pop();
+            currUI.SetActive(false);
+        }
+    }
+
+    void PopupOn1()
+    {
+        popupUIs[0].SetActive(true);
+        uiStack.Push(popupUIs[0]);
+    }
+    void PopupOn2()
+    {
+        popupUIs[1].SetActive(true);
+        uiStack.Push(popupUIs[1]);
+    }
+    void PopupOn3()
+    {
+        popupUIs[2].SetActive(true);
+        uiStack.Push(popupUIs[2]);
+    }
+}
