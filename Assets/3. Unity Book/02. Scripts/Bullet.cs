94a4b470f995f5efd05931d5e031c42a2a5533f9
GWONSEOYEON <sykwon1004@naver.com>
2025-07-16 00:02:14 +0900

    교과서 FPS 간단 개념 연습
    p96~p140?

---
+++ Assets/3. Unity Book/02. Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5;
+
+    public GameObject bulletFactory;
+    public GameObject firePosition;
+
+    void Update()
+    {
+        Vector3 dir = Vector3.up;
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/3. Unity Book/02. Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5;
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+
+        transform.position += Vector3.right * 5 * Time.deltaTime;
+
+        if (Input.GetButtonDown("Fire1"))
+            Debug.Log("마우스 왼쪽버튼 클릭");
+
+        if (Input.GetButtonDown("0"))
+            Debug.Log("마우스 왼쪽버튼 클릭");
+
+        if (Input.GetKeyDown("Space"))
+            Debug.Log("스페이스바 클릭");
+    }
+
+}
