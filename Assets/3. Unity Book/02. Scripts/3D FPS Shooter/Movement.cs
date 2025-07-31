0ef78922480929f9e6356fc97c93c33035a0ab41
GWONSEOYEON <sykwon1004@naver.com>
2025-07-31 16:21:05 +0900

    FPS 교과서 끝
    애니메이션 믹사모 사이트 리깅

---
+++ Assets/3. Unity Book/02. Scripts/3D FPS Shooter/Movement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);//크기와 방향이 있는 벡터
+        dir.Normalize();//방향만 있는 벡터 (방향을갖고 크기가 1)
+
+        transform.position += dir * Time.deltaTime * 5f;
+    }
+}
---
+++ Assets/3. Unity Book/02. Scripts/Study Emitters.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class StudyEmitters : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public SignalReceiver receiver;
+    public SignalAsset signal;
+
+    private void Start()
+    {
+        SetSignalEvent();
+    }
+    public void OnTimeLineSpeed(float speed)
+    {
+        //타임라인의 속도제어
+        timeline.playableGraph.GetRootPlayable(0).SetSpeed(speed);
+    }
+    //시그널에 이벤트를 등록하는 함수
+    public void SetSignalEvent()
+    {
+        UnityEvent eventContainer = new UnityEvent();//이벤트를 담는 변수
+
+        eventContainer.AddListener(() => OnTimeLineSpeed(0.2f));//이벤트 등록
+
+        receiver.AddReaction(signal, eventContainer);//signal에 Event 연결
+    }
+}
